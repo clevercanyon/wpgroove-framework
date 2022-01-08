3fa47dc957d0939445e99acfb97bec3266e2afcc
Mike Taykle <miketaykle@o5p.me>
2022-01-08 16:44:45 -0500

    update

--- dev/toolchain/composer/.cli-project-dir.php
+++ dev/toolchain/composer/.cli-project-dir.php
@@ -34,36 +34,9 @@ if ( 'cli' !== PHP_SAPI ) {
  * Locates project directory.
  *
  * @since 2021-12-15
- *
- * @throws \Exception On any failure.
- * @returns string Absolute project directory path.
  */
-return ( function () : string {
-	global $argv;
-
-	// Potentially relative.
-	$script_file = $argv[ 0 ];
-	$script_dir  = dirname( $script_file );
-
-	// Confirm that we are starting from a good location.
-	// If either of these are false we have a serious problem.
-	if ( ! is_file( $script_file ) || ! is_dir( $script_dir ) ) {
-		throw new \Exception( 'Failed to acquire script file|directory: `' . $script_file . '`.' );
-	}
-	// Look for nearest project directory that's not a symlink.
-	// Symlinks are bypassed because we use them locally for development.
-	// e.g., `[project-dir]/vendor/clevercanyon/[project-symlink]/composer.json`.
-	for ( $_i = 0; $_i <= 25; $_i++ ) {
-		$_project_file          = $script_dir . str_repeat( '/..', $_i ) . '/composer.json';
-		$_project_autoload_file = dirname( $_project_file ) . '/vendor/autoload.php';
-
-		if ( is_file( $_project_file ) && is_file( $_project_autoload_file ) ) {
-			$_project_dir = dirname( $_project_file );
-
-			if ( ! is_link( $_project_dir ) ) {
-				return realpath( $_project_dir );
-			}
-		}
-	} // Throw exception on any failure.
-	throw new \Exception( 'Failed to acquire project directory.' );
-} )();
+if ( is_dir( dirname( __FILE__, 4 ) . '/vendor' ) ) {
+	return require dirname( __FILE__, 4 ) . '/vendor/clevercanyon/php-js-utilities/dev/toolchain/composer/.cli-project-dir.php';
+} else { // Not root. We're inside a /vendor directory.
+	return require dirname( __FILE__, 7 ) . '/vendor/clevercanyon/php-js-utilities/dev/toolchain/composer/.cli-project-dir.php';
+}
